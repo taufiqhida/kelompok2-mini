b70cf69598df4bc110f3504e7cd9f16fcf46bc8b
GilangAlfarizi <89723613+GilangAlfarizi@users.noreply.github.com>
2023-11-13 17:01:01 +0700

    add controller + router

---
+++ routes/orderItems.router.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const router = express.Router()
+const controller = require("../controllers/orderItems.controller");
+
+router.post("/create", controller.create)
+router.put("/update/:id", controller.update)
+router.get("/", controller.getAll)
+router.get("/:id", controller.getId)
+router.delete("/:id", controller.delete)
+
+module.exports = router
